fd5084d63c10b50b2fb4f930993f5973d0a9c4d2
CedricCyber <104404794+CedricCyber@users.noreply.github.com>
2022-12-30 07:24:28 -0600

    attempt to render chip on rndrBrd from gmBrd clik

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // DOM Elements
 let gameBoard = document.getElementById("game-board");
+let renderBoard = document.getElementById("render-board");
 let player1turn = true;
 let player2turn = false;
 // Create JS Game Board
@@ -9,16 +10,27 @@ for (let i = 0; i < 42; i++) {
   box.classList = "";
   box.attributes.index = i;
   gameBoard.appendChild(box);
+  let renderBox = document.createElement("div");
+  renderBox.attributes.index = i;
+  renderBoard.appendChild(renderBox);
+
   let redChipLarge = document.createElement("img");
+  redChipLarge.classList = "";
   redChipLarge.src = "./images/counter-red-large.svg";
   let yellowChipLarge = document.createElement("img");
   yellowChipLarge.src = "./images/counter-yellow-large.svg";
   function addChip() {
     if (player1turn === true) {
+      let targetIndex = event.target.index;
       return (
-        event.target.appendChild(redChipLarge),
-        (player1turn = false),
-        (player2turn = true)
+        // event.target.parentElement.nextElementSibling.children[
+        //   targetIndex
+        // ].appendChild(redChipLarge)(
+          // event.target.appendChild(redChipLarge),
+          (player1turn = false)
+        ),
+        (player2turn = true),
+        console.log(event)
       );
     } else if (player2turn === true) {
       return (
