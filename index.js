9ed9ea7cd5191936af5ec27bedfcd2438eb8f221
CedricCyber <104404794+CedricCyber@users.noreply.github.com>
2023-01-04 05:17:04 -0600

    Created game logic for placing game peices

--- index.js
+++ index.js
@@ -10,17 +10,49 @@ const renderBoard = document.getElementById("render-board");
 const renderBoxes = document.querySelectorAll("#render-board div");
 console.log(renderBoxes);
 // variables for game logic
-let player1turn = true;
-let player2turn = false;
+let player1Turn = true;
+let player2Turn = false;
+let redChipLarge = [];
+let yellowChipLarge = [];
 // function to addChip to renderboard when gameboard is clicked
 // & adds game logic with conditional statements
 // for loop to loop through grid cells
 for (let i = 0; i < boxes.length; i++) {
   boxes[i].addEventListener("click", addChip);
   boxes[i].index = i;
+  // created 42 red and yellow chips in an array to append to renderBoard when gameBoard is clicked
+  redChipLarge.push(document.createElement("img"));
+  redChipLarge[i].src = "./images/counter-red-large.svg";
+  yellowChipLarge.push(document.createElement("img"));
+  yellowChipLarge[i].src = "./images/counter-yellow-large.svg";
 }
+console.log(redChipLarge[0]);
 function addChip() {
-  if (player1turn === true) {
-    return console.log(event.target.index);
+  // index of on click variable
+  let boxIndex = event.target.index;
+  if (
+    player1Turn == true &&
+    (boxes[boxIndex].classList.contains("open-space") ||
+      boxes[boxIndex + 7].classList.contains("taken"))
+  ) {
+    return (
+      (player1Turn = false),
+      (player2Turn = true),
+      renderBoxes[boxIndex].appendChild(redChipLarge[boxIndex]),
+      boxes[boxIndex].classList.add("taken")
+    );
+  } else if (
+    player2Turn == true &&
+    (boxes[boxIndex].classList.contains("open-space") ||
+      boxes[boxIndex + 7].classList.contains("taken"))
+  ) {
+    return (
+      (player1Turn = true),
+      (player2Turn = false),
+      renderBoxes[boxIndex].appendChild(yellowChipLarge[boxIndex]),
+      boxes[boxIndex].classList.add("taken")
+    );
+  } else {
+    return alert("Cant got here");
   }
 }
